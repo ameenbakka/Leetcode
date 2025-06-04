bce1de78c7e0ca43850ad397712abcd4c3acdbb5
Ameen Sahil M <97309035+ameenbakka@users.noreply.github.com>
2025-06-04 10:14:42 +0530

    Time: 0 ms (100%), Space: 39.6 MB (82%) - LeetHub

---
+++ 2556-convert-the-temperature/2556-convert-the-temperature.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public double[] ConvertTemperature(double celsius) {
+        double[] res = new double[2];
+        res[0]=celsius+273.15;
+        res[1]=(celsius*1.80)+32.00;
+        return res;
+    }
+}
