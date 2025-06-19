6f1d0efa32efae2abd82e166b5d9042fde0d16d4
Ameen Sahil M <97309035+ameenbakka@users.noreply.github.com>
2025-06-19 10:02:43 +0530

    Time: 0 ms (100%), Space: 46.5 MB (74.85%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+         int n = digits.Length;
+        for (int i = n - 1; i >= 0; --i) {
+            ++digits[i];
+            digits[i] %= 10;
+            if (digits[i] != 0) {
+                return digits;
+            }
+        }
+        digits = new int[n + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
